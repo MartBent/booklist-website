1e323170eef271bf2ac92ccb925f80341ce5f361
Mart Bent <martbent99@gmail.com>
2023-02-02 21:38:18 +0100

    Working with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { Book } from './components/book'
+import { Book, getBooks } from './components/book'
+
+let initialBooks = getBooks();
 
 function App() {
+
+    const [books, setBooks] = useState(initialBooks);
+
+    function updateBooks() {
+        const newBooks = books.map(book => {
+            if (book.counter == 0) {
+                return book;
+            }
+            return {
+                id: book.id,
+                title: book.title,
+                counter: book.counter - 1,
+            }
+        });
+        setBooks(newBooks);
+    };
+
+    useEffect(() => {
+        updateBooks();
+    }, []);
+
     return (
         <div className="App">
-            <Book title="Yeet"></Book>
-            <Book title="Second"></Book>
-        </div>
+            <button onClick={() => updateBooks()}>Update</button>
+            {console.log(books)}
+            {
+                books.map(book => {
+                    return <Book key={book.id} title={book.title} count={book.counter}></Book>
+                })
+            }
+        </div >
     )
 }
 
--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,5 +1,18 @@
+export function getBooks() {
+    let result = [];
+    for (let i = 0; i < 10; i++) {
+        let book = {
+            id: i,
+            title: "Book " + i,
+            counter: Math.floor(Math.random() * 100)
+        };
+        result[i] = book;
+    }
+    return result;
+}
 export function Book(props) {
-    return <div>
-        <h1>{props.title}</h1>
+    return <div >
+        <h3>{props.title}</h3>
+        <h5>{props.count}</h5>
     </div>;
 }
