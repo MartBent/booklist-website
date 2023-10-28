b01f93c8ed1ba5d1f11ac49ed58dfbffb27022f9
Mart Bent <=>
2023-10-28 13:45:39 +0200

    Added mantine component lib

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import '@mantine/core/styles.css';
+import { MantineProvider, createTheme } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+    <MantineProvider theme={createTheme({
+      fontFamily: 'Montserrat, sans-serif',
+      defaultRadius: 'md',
+    })}>
     <App />
-  //</React.StrictMode>,
+    </MantineProvider>
 )
